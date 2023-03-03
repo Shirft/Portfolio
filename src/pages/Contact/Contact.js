bff6050b653b6af1158e9caea756f5fee396cc36
Miguel Hilser <mygue0908@gmail.com>
2023-03-02 21:26:24 -0300

    cambios contact

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -7,14 +7,14 @@ const Contact = () => {
   const sendEmail = (event) => {
     event.preventDefault();
 
-    emailjs
+    const email = emailjs
       .sendForm(
         "service_08g8dtm",
         "template_2noa06a",
         event.target,
         "vrLRYebfXfNhuRbUN"
       )
-      .then((response) => {
+      /*.then((response) => {
         console.log(response);
         if (response.status == 200) {
           toast.success("Email sent!", {
@@ -25,15 +25,30 @@ const Contact = () => {
           });
           event.target.reset();
         }
+      })*/
+      .then((response) => {
+        console.log(response);
+        event.target.reset();
       })
       .catch((error) => {
         console.log(error);
-        toast.error("Email not sent!", {
+        /*toast.error("Email not sent!", {
           position: toast.POSITION.BOTTOM_CENTER,
           theme: "colored",
-        });
+        });*/
       });
-      
+    toast.promise(email, {
+      pending: {
+        render(){
+          return "Sending message..."
+        },
+        position: toast.POSITION.BOTTOM_CENTER,
+        theme: "colored",
+        autoClose: 1000,
+        },
+      success: "Email sent!",
+      error: "Email not sent!",
+    });
   };
 
   return (
@@ -42,18 +57,24 @@ const Contact = () => {
         <h1 className="contact-title-form">Contact me</h1>
         <form className="form-mail" onSubmit={sendEmail}>
           <label>Name</label>
-          <input type="text" name="user_name" required/>
+          <input type="text" name="user_name" required />
 
           <label>Email</label>
-          <input type="email" name="user_email" required/>
+          <input type="email" name="user_email" required />
 
           <label>Message</label>
-          <textarea name="user_message" id="" cols="30" rows="10" required></textarea>
+          <textarea
+            name="user_message"
+            id=""
+            cols="30"
+            rows="10"
+            required
+          ></textarea>
 
           <button>Send</button>
         </form>
       </div>
-      <ToastContainer autoClose={1000}/>
+      <ToastContainer autoClose={1000} />
     </div>
   );
 };
